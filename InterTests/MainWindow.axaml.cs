4f9bee8a9af8439a7b5d9d9722ca9af7cb1b9e4a
Daniel <dpenalba@codicesoftware.com>
2023-03-21 12:00:02 +0100

    Add project files.

---
+++ InterTests/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace InterTests
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
